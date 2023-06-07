fd0f86c9cde2a4f113cf51550a6deedbfd825d4b
obed smart <codingwithsmart@gmail.com>
2023-06-07 15:06:37 +0100

    update revert dive

---
+++ app.js
@@ -0,0 +1,6 @@
+const inputdate = document.getElementById("input-date");
+const inputselect = document.getElementById("input-select");
+const inputnumber = document.getElementById("input-number");
+const  inputdescrip = document.getElementById("input-descrip");
+const trackerButton = document.getElementById("track-btn");
+
