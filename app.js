09c4290d7d657dd391fee005d898c0ea332957bd
obed smart <codingwithsmart@gmail.com>
2023-06-10 15:29:39 +0100

    updated code
    1.in this commit i added i javascript function that add input.value into the <td></td>
    2. added some html changes like modifing the div and removing the form

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 const inputdate = document.getElementById("input-date");
 const inputselect = document.getElementById("input-select");
 const inputnumber = document.getElementById("input-number");
-const  inputdescrip = document.getElementById("input-descrip");
+const inputdescrip = document.getElementById("input-descrip");
 const trackerButton = document.getElementById("track-btn");
+const myTable = document.getElementById("tacker-table");
+const tablebody = document.getElementsByTagName("tbody")[0];
 
+trackerButton.addEventListener("click",()=>{
+
+    myTable.appendChild(tablebody);
+    let tableRow = myTable.insertRow();
+    let tableCell1 = tableRow.insertCell();
+    let tableCell2 = tableRow.insertCell();
+    let tableCell3 = tableRow.insertCell();
+    let tableCell4 = tableRow.insertCell();
+
+tableCell1.innerHTML = inputdate.value;
+tableCell2.innerHTML = inputselect.value;
+tableCell3.innerHTML = inputnumber.value;
+tableCell4.innerHTML = inputdescrip.value;
+
+inputdate.value = ""; 
+inputselect.value = ""; 
+inputnumber.value = ""; 
+inputdescrip.value = ""; 
+console.log(tableCell1)
+
+})
+
+if(tableCell1 === "" ){
+    tableCell1.style.background="red"
+}
+
+// const addtracker = ()=>{
+  
+
+// alert(myTable)
+    
+// }
