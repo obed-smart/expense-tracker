c1282e31967ef4111cb965695dc8e9b23c7aad2b
obed smart <codingwithsmart@gmail.com>
2023-06-12 19:34:21 +0100

    added a delete function to my javascript

--- app.js
+++ app.js
@@ -4,35 +4,38 @@ const inputnumber = document.getElementById("input-number");
 const inputdescrip = document.getElementById("input-descrip");
 const trackerButton = document.getElementById("track-btn");
 const myTable = document.getElementById("tacker-table");
-const tablebody = document.getElementsByTagName("tbody")[0];
+const tablebody = myTable.getElementsByTagName("tbody")[0];
 
 trackerButton.addEventListener("click",()=>{
 
-    myTable.appendChild(tablebody);
-    let tableRow = myTable.insertRow();
-    let tableCell1 = tableRow.insertCell();
-    let tableCell2 = tableRow.insertCell();
-    let tableCell3 = tableRow.insertCell();
-    let tableCell4 = tableRow.insertCell();
+    
+
+    let newRow = myTable.insertRow();
+    newRow.className = "list";
+    let tableCell1 = newRow.insertCell();
+    let tableCell2 = newRow.insertCell();
+    let tableCell3 = newRow.insertCell();
+    let tableCell4 = newRow.insertCell();
 
 tableCell1.innerHTML = inputdate.value;
 tableCell2.innerHTML = inputselect.value;
 tableCell3.innerHTML = inputnumber.value;
 tableCell4.innerHTML = inputdescrip.value;
 
-inputdate.value = ""; 
-inputselect.value = ""; 
-inputnumber.value = ""; 
-inputdescrip.value = ""; 
-console.log(tableCell1)
+
+// inputdate.value = ""; 
+// inputselect.value = ""; 
+// inputnumber.value = ""; 
+// inputdescrip.value = ""; 
+
+let deletebutton = document.createElement("span");
+    deletebutton.innerHTML ="✖"
+    newRow.appendChild(deletebutton);
+    deletebutton.addEventListener("click",(e)=>{
+        if(e.target.tagName === "SPAN")
+    e.target.parentElement.remove();
+        })
+    
 
 })
 
-
-
-// const addtracker = ()=>{
-  
-
-// alert(myTable)
-    
-// }
