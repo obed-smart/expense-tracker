2f0e7ea5f062e86087d715748095582153daaf41
obed smart <codingwithsmart@gmail.com>
2023-06-13 12:00:26 +0100

    this an update of the delete function and input area

--- app.js
+++ app.js
@@ -6,11 +6,33 @@ const trackerButton = document.getElementById("track-btn");
 const myTable = document.getElementById("tacker-table");
 const tablebody = myTable.getElementsByTagName("tbody")[0];
 
-trackerButton.addEventListener("click",()=>{
 
     
 
-    let newRow = myTable.insertRow();
+trackerButton.addEventListener("click",()=>{
+
+   const datevalue = inputdate.value;
+   const selectvalue =  inputselect.value;
+    const numbervalue =  inputnumber.value;
+   const descripvalue = inputdescrip.value;
+
+   if(datevalue || selectvalue || numbervalue || descripvalue !=="" ){
+    createTd();
+   }
+
+})
+document.body.addEventListener("keypress", (e)=>{
+    if(e.key === "H"){
+        trackerButton.style.display = "none";
+    }else if(e.key === "S"){
+        trackerButton.style.display = "block";
+    }else if (e.key === "A" ){
+
+    }
+})
+
+let createTd = ()=>{
+let newRow = myTable.insertRow();
     newRow.className = "list";
     let tableCell1 = newRow.insertCell();
     let tableCell2 = newRow.insertCell();
@@ -23,19 +45,16 @@ tableCell3.innerHTML = inputnumber.value;
 tableCell4.innerHTML = inputdescrip.value;
 
 
-// inputdate.value = ""; 
-// inputselect.value = ""; 
-// inputnumber.value = ""; 
-// inputdescrip.value = ""; 
+inputdate.value = ""; 
+inputselect.value = ""; 
+inputnumber.value = ""; 
+inputdescrip.value = ""; 
 
 let deletebutton = document.createElement("span");
     deletebutton.innerHTML ="✖"
-    newRow.appendChild(deletebutton);
+    tableCell4.appendChild(deletebutton);
     deletebutton.addEventListener("click",(e)=>{
         if(e.target.tagName === "SPAN")
-    e.target.parentElement.remove();
+    e.target.tablebody.remove();
         })
-    
-
-})
-
+}
